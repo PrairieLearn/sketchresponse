f65b61396713a0b9f95ea188d9871601ccbb73b0
Peter Stenger <peter@prairielearn.com>
2026-01-13 16:58:07 -0500

    add CI, uv, __init__ files

---
+++ sketchresponse/__init__.py
@@ -0,0 +1,5 @@
+"""SketchResponse - A tool for drawing and evaluating mathematical functions."""
+
+from .sketchresponse import grader, config, GradeableCollection
+
+__all__ = ["grader", "config", "GradeableCollection", "sketchresponse"]
--- sketchresponse/grader_lib/__init__.py
+++ sketchresponse/grader_lib/__init__.py
@@ -1,1 +1,25 @@
-# __all__ = ["MultipleSplinesFunction", "SplineFunction", "CurveFunction", "datalayer"]
+"""Grader library for SketchResponse."""
+
+from .GradeableFunction import GradeableFunction
+from .Asymptote import Asymptote, Asymptotes, VerticalAsymptotes, HorizontalAsymptotes
+from .LineSegment import LineSegment, LineSegments
+from .PolyLine import PolyLines
+from .Polygon import Polygon, Polygons
+from .Point import Point
+from .Tag import Tag, Tagables
+from .Gradeable import Gradeable
+from .Axis import Axis
+from .PolarTransform import PolarTransform
+
+__all__ = [
+    "GradeableFunction",
+    "Asymptote", "Asymptotes", "VerticalAsymptotes", "HorizontalAsymptotes",
+    "LineSegment", "LineSegments",
+    "PolyLines",
+    "Polygon", "Polygons",
+    "Point",
+    "Tag", "Tagables",
+    "Gradeable",
+    "Axis",
+    "PolarTransform",
+]
