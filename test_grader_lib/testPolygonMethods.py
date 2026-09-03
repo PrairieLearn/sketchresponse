c961a3cb3e7ca327f53d4c9077ccd27960cb32d4
Serena Caraco <15524864+SybelBlue@users.noreply.github.com>
2026-09-03 11:00:22 -0700

    Fix polygon grading with vertical edges

--- test_grader_lib/testPolygonMethods.py
+++ test_grader_lib/testPolygonMethods.py
@@ -1,5 +1,7 @@
 import unittest
 
+from sympy.geometry import Segment
+
 from sketchresponse.grader_lib import Polygon
 from test_grader_lib import TestData
 
@@ -178,6 +180,80 @@ class TestPolygonMethods(TestData.TestData):
             poly.polygons[0], [[-2, 0], [-3, -3]]
         )
         self.assertEqual(len(intersections), 0)
+
+    def test_segment_range_intersection(self):
+        data = self.load_as_gradeable_collections("polygon_point")
+        d = data[0]
+        args = d["pl"]
+        poly = Polygon.Polygons(
+            args.grader, args.submission, args.submission["meta"]["config"], args.tool_id
+        )
+
+        for segment_in_range in (poly.segment_in_range, poly.segment_in_range_strict):
+            with self.subTest(method=segment_in_range.__name__):
+                self.assertFalse(segment_in_range(Segment((-2, 0), (-2, 1)), -1, 1))
+                self.assertFalse(segment_in_range(Segment((2, 0), (2, 1)), -1, 1))
+                self.assertTrue(segment_in_range(Segment((-1, 0), (-1, 1)), -1, 1))
+                self.assertTrue(segment_in_range(Segment((1, 0), (1, 1)), -1, 1))
+                self.assertTrue(segment_in_range(Segment((-2, 0), (2, 0)), -1, 1))
+                self.assertTrue(segment_in_range(Segment((2, 0), (-2, 0)), -1, 1))
+
+    def test_cut_vertical_segments(self):
+        data = self.load_as_gradeable_collections("polygon_point")
+        d = data[0]
+        args = d["pl"]
+        poly = Polygon.Polygons(
+            args.grader, args.submission, args.submission["meta"]["config"], args.tool_id
+        )
+
+        self.assertIsNone(poly.cut_segment(Segment((-2, 0), (-2, 1)), -1, 1))
+        self.assertIsNone(poly.cut_segment(Segment((2, 0), (2, 1)), -1, 1))
+        self.assertEqual(
+            set(poly.cut_segment(Segment((-1, 0), (-1, 1)), -1, 1).points),
+            set(Segment((-1, 0), (-1, 1)).points),
+        )
+        self.assertEqual(
+            set(poly.cut_segment(Segment((1, 0), (1, 1)), -1, 1).points),
+            set(Segment((1, 0), (1, 1)).points),
+        )
+        self.assertEqual(
+            set(poly.cut_segment(Segment((0, -3), (0, 3)), -1, 1).points),
+            set(Segment((0, -2.5), (0, 2.5)).points),
+        )
+
+    def test_ltgt_function_ignores_vertical_edges_outside_range(self):
+        data = self.load_as_gradeable_collections("polygon_point")
+        d = data[0]
+        args = d["pl"]
+
+        for method, grader_type, points, func in (
+            (
+                "gt_function",
+                "greater-than",
+                [(0, 1.5), (2, 1.5), (2, 2), (0, 2)],
+                lambda x: x**2,
+            ),
+            (
+                "lt_function",
+                "less-than",
+                [(0, -2), (2, -2), (2, -1.5), (0, -1.5)],
+                lambda x: -(x**2),
+            ),
+        ):
+            with self.subTest(method=method):
+                args.grader["type"] = grader_type
+                args.grader["tolerance"] = 0
+                poly = Polygon.Polygons(
+                    args.grader,
+                    args.submission,
+                    args.submission["meta"]["config"],
+                    args.tool_id,
+                )
+                polygon = Polygon.Polygon(points)
+                poly.set_polygon_range_defined(polygon)
+                poly.polygons = [polygon]
+
+                self.assertTrue(getattr(poly, method)(func, -1, 1, 0))
 
 
 if __name__ == "__main__":
