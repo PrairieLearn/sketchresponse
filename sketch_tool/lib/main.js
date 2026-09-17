99aaef99de7aa87aecc001212a2fcf97b20cddc0
Nico Ritschel <n.ritschel@icloud.com>
2026-09-17 15:15:53 -0600

    add viewbox scaling

--- sketch_tool/lib/main.js
+++ sketch_tool/lib/main.js
@@ -79,9 +79,10 @@ export default class SketchInput {
   }
 
   init(plugins) {
+    this.el.style.setProperty('--si-width', `${this.params.width}px`);
     this.el.innerHTML = `
       <menu id="${this.id}-si-toolbar" class="si-toolbar${this.params.readonly ? ' disable' : ''}"></menu>
-      <svg id="${this.id}-si-canvas" class="si-canvas" touch-action="none" width="${this.params.width}" height="${this.params.height}">
+      <svg id="${this.id}-si-canvas" class="si-canvas" touch-action="none" viewBox="0 0 ${this.params.width} ${this.params.height}" width="${this.params.width}" height="${this.params.height}">
         <rect width="100%" height="100%" fill="#F0F0F0" />
         <rect x="${this.config.safetyBuffer}" y="${this.config.safetyBuffer}" width="${this.config.width - 2 * this.config.safetyBuffer}" height="${this.config.height - 2 * this.config.safetyBuffer}" fill="white" />
       </svg>
@@ -446,6 +447,28 @@ export default class SketchInput {
     });
 
     if (this.initialState) this.messageBus.emit('loadInitialState');
+
+    if (!this.params.readonly) {
+      const container = this.el;
+      const toolbar = this.toolbar.el;
+      // Zoom also scales the toolbar's layout height, keeping the canvas below it.
+      let resizeFrame;
+      const resizeObserver = new ResizeObserver(() => {
+        // Apply zoom after observer delivery because it changes the container height.
+        cancelAnimationFrame(resizeFrame);
+        resizeFrame = requestAnimationFrame(() => {
+          if (toolbar.offsetWidth > 0) {
+            const zoom = Math.min(
+              1,
+              container.clientWidth / toolbar.offsetWidth,
+            );
+            toolbar.style.zoom = zoom;
+          }
+        });
+      });
+      resizeObserver.observe(container);
+      resizeObserver.observe(toolbar);
+    }
 
     this.messageBus.emit('ready');
   }
